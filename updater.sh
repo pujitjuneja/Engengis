9a109e14503193df1239d02c3c256c9963923ae3
root <root@jake-Aspire-7741.(none)>
2012-01-21 16:21:23 +0100

    Engengis.Delta OTA service

--- updater.sh
+++ updater.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 # Copyright© 2011 redmaner
-# Engengis updater V15
+# Engengis updater V16
 
-
+checkupdate () {
 clear
 CONFIG=/data/engengis.conf
-VERSION=44
+VERSION=46
 OLDVERSION=`cat /system/etc/engengis/version`
 
 echo
@@ -17,9 +17,9 @@ echo "Latest version = Engengis.Charlie_$VERSION"
 echo
 if [ $OLDVERSION -eq $VERSION ]; then
      echo "Update is not needed"
-     sleep 3
-     clear
-     engengis
+     echo "Do you want to force an update?"
+     echo "[y or n]"
+     read forceupdate
 elif [ $OLDVERSION -lt $VERSION ]; then
      echo "There is an update available"
      echo "Do you want to update?"
@@ -50,6 +50,7 @@ case "$update" in
   rm -f /system/bin/engengiss
   rm -f /system/xbin/sqlite3
   rm -f /system/xbin/zipalign
+  rm -f /system/etc/init.d/S00ramscript
   rm -f /system/etc/init.d/S07hsstweak
   rm -f /system/etc/init.d/S14zipalign
   rm -f /system/etc/init.d/S21sqlite
@@ -58,11 +59,12 @@ case "$update" in
   rm -f /system/etc/init.d/S42cpugovernor
   rm -f /system/etc/init.d/S49dropcaches
   rm -f /system/etc/init.d/S56internet
+  rm -f /system/etc/init.d/S63internetsecurity
   rm -f /data/engengis.log
   echo "Installing new files..."
   mkdir /system/etc/engengis
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/bin/engengis -O /system/bin/engengis
-  wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S07hsstweak -O /system/etc/engengis/S07hsstweak
+  wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S07hsstweak -O /system/etc/engengis/S00ramscript
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S14zipalign -O /system/etc/engengis/S14zipalign
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S21hsstweak -O /system/etc/engengis/S21hsstweak
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S28bfqscheduler -O /system/etc/engengis/S28bfqscheduler
@@ -84,6 +86,7 @@ case "$update" in
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S42cpusmartassv2 -O /system/etc/engengis/S42cpusmartassv2
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S49dropcaches -O /system/etc/engengis/S49dropcaches
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S56internet -O /system/etc/engengis/S56internet
+  wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S63internetsecurity -O /system/etc/engengis/S63internetsecurity
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/version -O /system/etc/engengis/version
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/xbin/sqlite3 -O /system/xbin/sqlite3
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/xbin/zipalign -O /system/xbin/zipalign
@@ -108,6 +111,16 @@ case "$update" in
   engengis 
   ;;
 esac
+
+case "$forceupdate" in
+  "y" | "Y")
+  echo "1" > $OLDVERSION
+  clear; checkupdate;;
+  "n" | "N") clear; engengis;;
+esac
+}
+
+checkupdate;
 
 
 
