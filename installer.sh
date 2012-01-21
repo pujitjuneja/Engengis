dcf78629f8f12d34ebb49b248292cf379c259c34
root <root@jake-Aspire-7741.(none)>
2012-01-21 16:27:55 +0100

    Engengis.Delta installer

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 # Copyright© 2011 redmaner
-# Engengis installer V5
+# Engengis installer V6
 
 clear
 echo
@@ -17,7 +17,8 @@ echo "[ y or n]"
 read option
 
 case "$option" in 
-  "y" | "Y")
+ 
+  mount -o remount,rw /system
   echo
   echo "This will take a moment..."
   mount -o remount,rw /system
@@ -31,6 +32,7 @@ case "$option" in
   rm -f /system/bin/engengiss
   rm -f /system/xbin/sqlite3
   rm -f /system/xbin/zipalign
+  rm -f /system/etc/init.d/S00ramscript
   rm -f /system/etc/init.d/S07hsstweak
   rm -f /system/etc/init.d/S14zipalign
   rm -f /system/etc/init.d/S21sqlite
@@ -39,11 +41,12 @@ case "$option" in
   rm -f /system/etc/init.d/S42cpugovernor
   rm -f /system/etc/init.d/S49dropcaches
   rm -f /system/etc/init.d/S56internet
+  rm -f /system/etc/init.d/S63internetsecurity
   rm -f /data/engengis.log
   echo "Installing new files..."
   mkdir /system/etc/engengis
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/bin/engengis -O /system/bin/engengis
-  wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S07hsstweak -O /system/etc/engengis/S07hsstweak
+  wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S07hsstweak -O /system/etc/engengis/S00ramscript
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S14zipalign -O /system/etc/engengis/S14zipalign
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S21hsstweak -O /system/etc/engengis/S21hsstweak
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S28bfqscheduler -O /system/etc/engengis/S28bfqscheduler
@@ -65,6 +68,7 @@ case "$option" in
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S42cpusmartassv2 -O /system/etc/engengis/S42cpusmartassv2
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S49dropcaches -O /system/etc/engengis/S49dropcaches
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S56internet -O /system/etc/engengis/S56internet
+  wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/S63internetsecurity -O /system/etc/engengis/S63internetsecurity
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/etc/engengis/version -O /system/etc/engengis/version
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/xbin/sqlite3 -O /system/xbin/sqlite3
   wget -q http://dl.dropbox.com/u/26139869/engengis/system/xbin/zipalign -O /system/xbin/zipalign
