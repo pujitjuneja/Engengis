f307c6fa3d03a0a1c5a9871f027f4c04a57d27ee
root <root@jake-Aspire-7741.(none)>
2012-01-21 19:12:15 +0100

    Build-Tool update

--- build-engengis.sh
+++ build-engengis.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Copyright (c) 2012, Jake "redmaner" van der Putten
 # Engengis build tool
+# Version: 21012012
 
 # System requirements
 # -----------------------------------------
 # - Ubuntu (or any other unix based system)
 # - Java (to sign zip file)
 # - zip/unzip (apt-get install zip/apt-get install unzip)
+
+VERSION=`cat currentversion`
 
 if [ -d build ]; then
      echo "Start building engengis..."
@@ -23,7 +26,7 @@ mv include/engengis.zip signzip/engengis.zip
 cd signzip
 java -jar signapk.jar testkey.x509.pem testkey.pk8 engengis.zip signed-engengis.zip
 cd ..
-mv signzip/signed-engengis.zip build/Engengis.Delta_build-.zip
+mv signzip/signed-engengis.zip build/$VERSION.zip
 echo "Done find build at:"
-echo "/build/Engengis.Delta_build-.zip"
+echo "/build/$VERSION.zip"
 rm -f signzip/engengis.zip
